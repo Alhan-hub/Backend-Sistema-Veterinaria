b80194092af9cb1fac67d25e52a2fb0bf5a6ad0a
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-21 15:58:58 -0500

    feat: tipo de retorno del constructor y atributo email

--- src/entities/Propietario.py
+++ src/entities/Propietario.py
@@ -1,8 +1,9 @@
 class Propietario:
-    def __init__(self, nombre:str, documento:str, telefono:str):
+    def __init__(self, nombre:str, documento:str, telefono:str, email:str) -> None:
         self._nombre = nombre.strip()
         self._documento = documento.strip()
         self._telefono = telefono.strip()
+        self._email = email.strip()
 
     @property
     def nombre(self) -> str:
@@ -16,5 +17,9 @@ class Propietario:
     def telefono(self) -> str:
         return self._telefono
     
+    @property
+    def email(self) -> str:
+        return self._email
+
     def __str__(self) -> str:
         return f"Propietario: {self._nombre}  Documento: {self._documento}"
