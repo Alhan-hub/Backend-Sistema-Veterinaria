d95a3a4b137377174b72b288e22463fa43784973
ALHAN RENDON RUIZ <alhanrendon1118935@correo.itm.edu.co>
2026-05-22 07:53:06 -0500

    Create conftest.py
    Creación de la carpeta tests y del conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.types import CHAR
+
+from main import app
+from src.database.config import Base, get_db
+
+@compiles(UUID, 'sqlite')
+def compile_uuid(type_, compiler, **kw):
+    return "CHAR(32)"
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture(scope="function")
+def db_session():
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    yield db
+    db.close()
+    Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture(scope="function")
+def client(db_session):
+    def override_get_db():
+        yield db_session
+            
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as test_client:
+        yield test_client
+    app.dependency_overrides.clear()
+
+tests/test_usuario.py
+import uuid
